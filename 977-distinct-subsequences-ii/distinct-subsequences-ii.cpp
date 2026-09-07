0d9cddddf534dcdbce7cc820bfc82747d98b03bf
Lokhnadh <lokhnadhgembali@gmail.com>
2026-09-07 19:55:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (62.00%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
+    int MOD=1e9+7;
     int distinctSubseqII(string s) {
-        int n = s.size();
-        int MOD = 1e9 + 7;
-
-        vector<int> dp(n, 1);
-        int result = 0;
-
-        for(int i = 0; i < n; i++){
-            // Try extending subsequences ending before i
-            for(int j = 0; j < i; j++){
-                if(s[i] != s[j]) {
-                    dp[i] = (dp[i] + dp[j]) % MOD;
-                }
-            }
-            result = (result + dp[i]) % MOD;
+        int n=s.size();
+        vector<long long> end(27, 0);
+        long long total=0;
+        for(int i=0; i<n; i++){
+            char ch=s[i];
+            long long new_cnt=(total+1)%MOD;
+            total=(total+new_cnt-end[ch-'a']+MOD)%MOD;
+            end[ch-'a']=new_cnt;
         }
-        return result;
+        return total;
     }
 };
