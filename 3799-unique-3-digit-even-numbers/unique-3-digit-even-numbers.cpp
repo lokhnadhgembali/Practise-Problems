92599527e17004763a39849626a6542a8745e1c5
Lokhnadh <lokhnadhgembali@gmail.com>
2026-09-11 20:27:33 +0530

    Time: 0 ms (100.00%) | Memory: 28.7 MB (93.75%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        unordered_set<int> seen;
-        int n = digits.size();
+        int cnt[10] = {};
 
-        for(int h = 0; h < n; h++){
-            if (digits[h] == 0) continue;
-            for(int t = 0; t < n; t++){
-                if (t == h) continue;
-                for(int u = 0; u < n; u++){
-                    if(u == h || u == t) continue;
-                    if(digits[u] % 2 != 0) continue;
+        for (int d : digits)
+            cnt[d]++;
 
-                    int num = digits[h]*100+digits[t]*10+digits[u];
-                    seen.insert(num);
+        int res = 0;
+
+        for (int a = 1; a <= 9; a++) {
+            if (cnt[a] == 0)
+                continue;
+
+            cnt[a]--;
+
+            for (int b = 0; b <= 9; b++) {
+                if (cnt[b] == 0)
+                    continue;
+
+                cnt[b]--;
+
+                for (int c = 0; c <= 8; c += 2) {
+                    if (cnt[c] > 0)
+                        res++;
                 }
+
+                cnt[b]++;
             }
+
+            cnt[a]++;
         }
 
-        return seen.size();
+        return res;
     }
 };
