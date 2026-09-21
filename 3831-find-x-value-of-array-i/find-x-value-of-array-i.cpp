6102cf81732addcb21f99cf63e27603cec950f33
Lokhnadh <lokhnadhgembali@gmail.com>
2026-09-21 20:41:47 +0530

    Time: 18 ms (97.32%) | Memory: 105.9 MB (98.21%) - LeetSync

--- 3831-find-x-value-of-array-i/find-x-value-of-array-i.cpp
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.cpp
@@ -1,43 +1,23 @@
+int freq[5], freq2[5];
 class Solution {
 public:
-    using ll = long long;
-
-    int n;
-
-    ll solve(int i, int prevProd, int req, int k, auto& nums, auto& dp) {
-
-        if(i >= nums.size()) return 0;
-        if(dp[i][prevProd] != -1) return dp[i][prevProd];
-
-        ll skip = 0, take = 0;
-
-        // Skip current element (only if no subarray started yet)
-        if(prevProd == k){
-            skip = solve(i + 1, k, req, k, nums, dp);
+    static vector<long long> resultArray(vector<int>& nums, int k) {
+        const int n=nums.size();
+        if (k==1) return {1LL*n*(n+1)/2};// special case
+        vector<long long> ans(k, 0);
+        memset(freq, 0, sizeof(int)*k);// freq[r]=how many times seen for x%k
+        for (int x: nums){
+            const int r=x%k;
+            memset(freq2, 0, sizeof(int)*k);
+            ans[r]++;
+            for (int j=0; j<k; j++){
+                const int prod=1LL*j*r%k;
+                freq2[prod]+=freq[j];
+                ans[prod]+=freq[j];
+            }
+            freq2[r]++;
+            memcpy(freq,freq2, sizeof(int)*k);
         }
-
-        // Take current element
-        ll curProd;
-        if(prevProd == k) curProd = nums[i]; // started from cur num
-        else curProd = (prevProd * nums[i]) % k; // extending forward
-
-        take += (curProd == req);
-        take += solve(i + 1, curProd, req, k, nums, dp);
-
-        return dp[i][prevProd] = take + skip;
-    }
-
-    vector<ll> resultArray(vector<int>& nums, int k) {
-        n = nums.size();
-        vector<ll> res(k, 0);
-
-        for(int& num : nums) num %= k;
-
-        for(int r = 0; r < k; r++){
-            vector<vector<ll>> dp(n, vector<ll>(k + 1, -1));
-            res[r] = solve(0, k, r, k, nums, dp);
-        }
-
-        return res;
+        return ans;
     }
 };
